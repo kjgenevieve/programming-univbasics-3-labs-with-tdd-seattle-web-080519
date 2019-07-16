f2bd852258955de9e598169679021cfe3f392fea
Genevieve <kj.genevieve@gmail.com>
2019-07-16 01:24:41 +0000

    Automatically backed up by Learn

---
+++ calculator.rb
@@ -0,0 +1,7 @@
+first_number
+second_number
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
